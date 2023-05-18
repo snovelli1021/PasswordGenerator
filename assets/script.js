21bf787cbbfbe00df9407354e8ec6d614147eff4
Stephen Novelli <snovelli1021@gmail.com>
2023-05-17 18:21:12 -0700

    Updated the javascript to print a new password with some basic validation based on user inputs for length and checkboxes.

--- assets/script.js
+++ assets/script.js
@@ -1,67 +1,43 @@
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXZY";
+  var number = "0123456789";
+  var specChar = "!@#$%^&*()";
 
-// Assignment code here
-var lowerCase = ("abcdefghijklmnopqrstuvwxyz");
-var upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXZY")
-var number = ("0123456789");
-var specChar = ("!@#$%^&*()");
+  //Reads the check/inputs for the created password.
+  var passwordLength = document.getElementById("passwordLength").value;
+  var checkLowercase = document.getElementById("checkLowercase").checked;
+  var checkUppercase = document.getElementById("checkUppercase").checked;
+  var checkNumbers = document.getElementById("checkNumbers").checked;
+  var checkSpecialChars = document.getElementById("checkSpecialChars").checked;
 
+  //Sets the conditions of the password length and characters.
+  var allowedChars = "";
+  if (passwordLength >= 8 && passwordLength <= 24) {
+    if (checkLowercase || checkUppercase || checkNumbers || checkSpecialChars) {
+      if (checkLowercase) allowedChars += lowerCase;
+      if (checkUppercase) allowedChars += upperCase;
+      if (checkNumbers) allowedChars += number;
+      if (checkSpecialChars) allowedChars += specChar;
 
-// Write password to the #password input
-function writePassword() {
+      var password = "";
+      for (var i = 0; i < passwordLength; i++) {
+        var randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+      }
 
-  var password = "";
-  var passwordText = "";
+      //Prints the password to the card element on page, as an alert, and in the console.
+      document.getElementById("password").value = password;
+      alert("This is your new password:   " + password);
+      console.log(password);
 
-//Sets the conditions of the password length and characters
-  let inputLength = prompt("Please choose how long you would like your password to be. It must be between 8 and 128 characters long.");
-  inputLength = parseInt(inputLength);
-
-  if (inputLength < 8){
-    alert("Password must be at least 8 characters.");
-    return "";
+      return true;
+    } else {
+      alert("Please select at least one checkbox.");
+    }
+  } else {
+    alert("Please pick a number between 8 and 24.");
   }
 
-  if (inputLength > 128) {
-    alert("Password may not exceed 128 characters.");
-    return "";
-  }
-
-  var inputLowerCase = confirm("Do you want lower case letters in your password?");
-
-  if (inputLowerCase) {
-      passwordText = lowerCase;
-  }
-
-  var inputUpperCase = confirm("Do you want any upper case letters?");
-
-  if(inputUpperCase) {
-    passwordText = passwordText.concat(upperCase)
-  }
-
-  var inputNumber = confirm("Do you want to add a number?");
-
-  if(inputNumber) {
-    passwordText = passwordText.concat(number)
-  }
-
-  var inputSpecChar = confirm("How about a special character?");
-
-  if(inputSpecChar) {
-    passwordText = passwordText.concat(specChar)
-  }
-
-  for (var i = 0; i < inputLength; i++) {
-    password = password + passwordText[Math.floor(Math.random() * passwordText.length)]
-  }
-
-//Prints the password to the console, as an alert, and the password box on page.
-passwordEl = document.querySelector("#password");
-passwordEl.textContent = password;
-alert("This is your new password:   " + password)
-console.log (password);
+  return false;
 }
-
-// Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
